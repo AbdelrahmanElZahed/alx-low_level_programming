c262804ffbb151dc456572b08b07ce5aa216d8d3
Abdelrahman <abdelrahmanelzahed94@gmail.com>
2024-06-26 19:55:47 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,7 +7,7 @@
  */
 void print_to_98(int n){
         if ( n <= 98){
-	       for (; n <= 98, n++){
+	       for (n = 0; n <= 98; n++){
 	       if ( n == 98){
 	printf("%d", n);	       
 	printf("\n");
@@ -19,7 +19,7 @@ void print_to_98(int n){
 	       }
 	}
 	else{
-		for(; n >=98, n--){
+		for(n = 0; n >=98; n--){
 			if(n == 98){
 				printf("%d", n);
 				printf("\n");
