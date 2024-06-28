b95d2c06585eb10fbf8cbda43b9678ec3ba5b072
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2024-06-28 22:53:13 +0300

    check for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,11 +4,14 @@
  * @c: input character
  * Return: (1) for true an (0) or false
  */
-int _isupper(int c){
-	if(c >= 'A' && c <= 'Z'){
-		return(1);
+int _isupper(int c) 
+{
+	if(c >= 'A' && c <= 'Z') 
+	{
+		return (1);
 	}
-	else{
-		return(0);
+	else 
+	{
+		return (0);
 	}
 }
