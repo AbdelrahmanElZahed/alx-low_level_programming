d36b6580eedc365f1b4f6e93f92726e8a4727c5c
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2024-07-03 09:55:00 +0300

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,42 +7,36 @@
  */
 void times_table(void)
 {
-    int a, b, c, d, e;
+	int a, b, c;
 
-    for (a = 0; a <= 9; a++)
-    {
-        for (b = 0; b <= 9; b++)
-        {
-            c = a * b;
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
 
-            if (c > 9)
-            {
-                d = c % 10;
-                e = (c - d) / 10;
+			if (b == 0)
+			{
+				_putchar(c + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
 
-                _putchar(',');
-                _putchar(' ');
-                _putchar(e + '0');
-                _putchar(d + '0');
-            }
-            else
-            {
-                if (b != 0)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-                else if (b == 0 && a != 0)
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-
-                _putchar(c + '0');
-            }
-        }
-        _putchar('\n');
-    }
+				if (c >= 10)
+				{
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(c + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
 }
 
