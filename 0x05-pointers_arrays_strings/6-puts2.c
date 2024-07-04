1698466be4a8d9756047a7833d345f57202ae60f
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2024-07-04 17:23:19 +0300

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,25 +3,22 @@
 /**
  * puts2 - prints every second character of a string starting with the first character
  * @str: input string
- * Return: void
+ *
+ * Description: Prints every second character of @str starting with the first character.
  */
 void puts2(char *str)
 {
-    int longi = 0;
-    int n;
+	int longi = 0;
+	int n;
 
-    /* Calculate the length of the string */
-    while (str[longi] != '\0')
-    {
-        longi++;
-    }
+	/* Calculate the length of the string */
+	while (str[longi] != '\0')
+		longi++;
 
-    /* Print every second character starting with the first one */
-    for (n = 0; n < longi; n += 2)
-    {
-        _putchar(str[n]);
-    }
+	/* Print every second character starting with the first one */
+	for (n = 0; n < longi; n += 2)
+		_putchar(str[n]);
 
-    _putchar('\n');
+	_putchar('\n');
 }
 
