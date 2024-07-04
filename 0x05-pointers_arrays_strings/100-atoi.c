e72ba6727f57e25bfa53789492b37bfe8d2ee83d
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2024-07-04 18:08:21 +0300

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
@@ -9,48 +8,33 @@
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int n = 0;
-	int sign = 1;
-	int digit_found = 0;
+    int i = 0;
+    int n = 0;
+    int sign = 1; /* Initialize sign to positive */
 
-	/* Skip initial non-numeric characters */
-	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9')
-		&& s[i] != '-' && s[i] != '+')
-	{
-		i++;
-	}
+    /* Skip whitespace characters */
+    while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == '\r')
+    {
+        i++;
+    }
 
-	/* Check for sign */
-	if (s[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	else if (s[i] == '+')
-	{
-		i++;
-	}
+    /* Check for sign */
+    while (s[i] == '-' || s[i] == '+')
+    {
+        if (s[i] == '-')
+        {
+            sign *= -1; /* Toggle sign */
+        }
+        i++;
+    }
 
-	/* Convert digits to integer */
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		if (n > (INT_MAX - (s[i] - '0')) / 10)
-		{
-			/* Handle overflow */
-			return ((sign == 1) ? INT_MAX : INT_MIN);
-		}
-		n = n * 10 + (s[i] - '0');
-		digit_found = 1;
-		i++;
-	}
+    /* Convert digits to integer */
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        n = n * 10 + (s[i] - '0');
+        i++;
+    }
 
-	/* Return 0 if no digits were found */
-	if (!digit_found)
-	{
-		return (0);
-	}
-
-	return (sign * n);
+    return sign * n;
 }
 
