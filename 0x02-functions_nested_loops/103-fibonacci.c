d397a0b0df1c46facd5061deadf050d7a6031884
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2024-07-04 16:05:51 +0300

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,21 +7,21 @@
  */
 int main(void)
 {
-    long int x = 1, y = 2, sum = 0, tsum = 0;
+	long int x = 1, y = 2, sum = 0, tsum = 0;
 
-    while (y <= 4000000)
-    {
-        if (y % 2 == 0)
-        {
-            tsum += y;
-        }
+	while (y <= 4000000)
+	{
+		if (y % 2 == 0)
+		{
+			tsum += y;
+		}
 
-        sum = x + y;
-        x = y;
-        y = sum;
-    }
+		sum = x + y;
+		x = y;
+		y = sum;
+	}
 
-    printf("%ld\n", tsum);
-    return (0);
+	printf("%ld\n", tsum);
+	return (0);
 }
 
