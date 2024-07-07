d33392586303eb36382d6deb6832b5a19a48c862
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2024-07-07 15:40:28 +0300

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
-#include<stdio.h>
-int main (void){
-puts("\"Programming is building a multilingual puzzle");
+#include <stdio.h>
+
+/**
+ * main - Prints out a string to stdout.
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
