1e463b27c8ec78a450d6b36525162e3a564c1648
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2024-07-08 13:52:05 +0300

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: (0) if success
+ */
+int main(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i += 1;
+	}
+	printf("\n");
+	return (0);
+}
