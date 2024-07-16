fb8617dd90e925500b3fabf953889e7fcd78b391
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2024-07-16 10:00:39 +0300

    It's not bragging if you can back it up

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * free_grid - Frees a 2D grid previously created by alloc_grid
@@ -8,6 +9,7 @@
  *
  * Return: Nothing
  */
+
 void free_grid(int **grid, int height)
 {
 	int i;
