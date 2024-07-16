ba4b38f16688883dfd42c411ca612472cd2f1e66
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2024-07-16 06:47:30 +0300

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,46 +1,75 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * argstostr - concatenates all the arguments of the program
- * @ac: argument count
- * @av: argument vector
+ * count_words - Counts the number of words in a string.
+ * @str: The string to count words from.
  *
- * Return: pointer to concatenated string
+ * Return: Number of words.
  */
-char *argstostr(int ac, char **av)
+int count_words(char *str)
 {
-	int i, j, total_length = 0;
-	char *str;
-	int k = 0;
+    int count = 0, in_word = 0;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
-	/* Calculate total length of arguments including newlines */
-	for (i = 0; i < ac; i++)
-	{
-		j = 0;
-		while (av[i][j])
-		{
-			total_length++;
-			j++;
-		}
-		total_length++; /* For the newline character */
-	}
-	/* Allocate memory for the concatenated string */
-	str = malloc((total_length + 1) * sizeof(char));
-	if (str == NULL)
-		return (NULL);
-	/* Copy arguments into str and add newlines */
-	for (i = 0; i < ac; i++)
-	{
-		j = 0;
-		while (av[i][j])
-		{
-			str[k++] = av[i][j++];
-		}
-		str[k++] = '\n';
-	}
-	str[k] = '\0'; /* Null-terminate the string */
-	return (str);
+    while (*str)
+    {
+        if (*str == ' ' || *str == '\t' || *str == '\n')
+        {
+            in_word = 0;
+        }
+        else if (in_word == 0)
+        {
+            in_word = 1;
+            count++;
+        }
+        str++;
+    }
+    return count;
 }
+
+/**
+ * strtow - Splits a string into words.
+ * @str: The string to split.
+ *
+ * Return: Pointer to an array of strings (words) or NULL on failure.
+ */
+char **strtow(char *str)
+{
+    char **words;
+    int word_count, i, j, word_len;
+
+    if (str == NULL || *str == '\0')
+        return NULL;
+
+    word_count = count_words(str);
+    words = malloc((word_count + 1) * sizeof(char *));
+    if (words == NULL)
+        return NULL;
+
+    for (i = 0; i < word_count; i++)
+    {
+        while (*str == ' ' || *str == '\t' || *str == '\n')
+            str++;
+
+        word_len = 0;
+        while (str[word_len] && str[word_len] != ' ' && str[word_len] != '\t' && str[word_len] != '\n')
+            word_len++;
+
+        words[i] = malloc((word_len + 1) * sizeof(char));
+        if (words[i] == NULL)
+        {
+            for (j = 0; j < i; j++)
+                free(words[j]);
+            free(words);
+            return NULL;
+        }
+
+        for (j = 0; j < word_len; j++)
+            words[i][j] = *str++;
+        words[i][j] = '\0';
+    }
+    words[i] = NULL;
+    return words;
+}
+
