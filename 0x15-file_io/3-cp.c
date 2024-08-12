9cd9cccaa2dd41549168dc95078bf725d1b100fa
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2024-08-13 01:24:48 +0300

    cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,87 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BUFFER_SIZE 1024
+
+void close_file(int fd);
+
+/**
+ * main - Entry point of the program that copies the content of one file to
+ *        another file.
+ * @argc: Number of arguments.
+ * @argv: Array of arguments.
+ *
+ * Return: 0 on success, exits with code on failure.
+ */
+int main(int argc, char *argv[])
+{
+    int fd_from, fd_to, rd_bytes, wr_bytes;
+    char buffer[BUFFER_SIZE];
+
+    /* Check for the correct number of arguments */
+    if (argc != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
+
+    /* Open the source file (file_from) */
+    fd_from = open(argv[1], O_RDONLY);
+    if (fd_from == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+
+    /* Open the destination file (file_to) */
+    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    if (fd_to == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+        close_file(fd_from);
+        exit(99);
+    }
+
+    /* Read from file_from and write to file_to in chunks of BUFFER_SIZE */
+    while ((rd_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+    {
+        wr_bytes = write(fd_to, buffer, rd_bytes);
+        if (wr_bytes != rd_bytes)
+        {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+            close_file(fd_from);
+            close_file(fd_to);
+            exit(99);
+        }
+    }
+
+    if (rd_bytes == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        close_file(fd_from);
+        close_file(fd_to);
+        exit(98);
+    }
+
+    /* Close the file descriptors */
+    close_file(fd_from);
+    close_file(fd_to);
+
+    return (0);
+}
+
+/**
+ * close_file - Closes a file descriptor and handles errors.
+ * @fd: The file descriptor to close.
+ */
+void close_file(int fd)
+{
+    if (close(fd) == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+        exit(100);
+    }
+}
+
