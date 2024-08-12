d602e5b16dc2562ad5c3cbda38b234201762405b
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2024-08-13 00:58:30 +0300

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,6 @@ int create_file(const char *filename, char *text_content)
 	{
 		while (text_content[length])
 			length++;
-        
 		/* Write the content to the file */
 		bytes_written = write(fd, text_content, length);
 		if (bytes_written == -1)
