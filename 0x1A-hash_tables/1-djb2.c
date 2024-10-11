4c503ab2ef50f0fb4090520592e7fb709160d95d
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2024-10-12 00:36:38 +0300

    1. djb2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,12 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Implements the djb2 hash function
+ * @str: The string to hash (unsigned char pointer)
+ *
+ * This function takes a string and hashes it using the djb2
+ * algorithm, which is a simple, efficient, and effective hashing
+ * function. It multiplies the hash by 33 for each character in the
+ * string and adds the ASCII value of the character.
+ *
+ * Return: The generated hash as an unsigned long int.
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
